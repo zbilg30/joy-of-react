e5937eadc905d823b4af6a58aae1afbdb6e6eb97
bilguunz <bilguun.z@techpack.mn>
2024-09-24 18:54:42 +0800

    onclick remove toast

--- toast/src/components/ToastShelf.jsx
+++ toast/src/components/ToastShelf.jsx
@@ -1,11 +1,15 @@
 import { useToast } from "../context/ToastContext";
 
 export const ToastShelf = () => {
-  const { toasts } = useToast();
+  const { toasts, removeToast } = useToast();
   return (
     <div className={`toast-shelf ${toasts.length === 0 && "hidden"}`}>
       {toasts.map((toast) => (
-        <div key={toast.id} className={`toast ${toast.variant}`}>
+        <div
+          key={toast.id}
+          onClick={() => removeToast(toast.id)}
+          className={`toast ${toast.variant}`}
+        >
           {toast.message}
         </div>
       ))}
